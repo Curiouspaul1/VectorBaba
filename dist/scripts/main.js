58d1504c9cc928b58b96e9ca607719a5c48a04a1
Curious Paul <170408025@live.unilag.edu.ng>
2020-07-11 11:37:11 +0100

    added stuyff

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -66,6 +66,49 @@ carousel.addEventListener('transitionend',()=>{
   }
 });*/
 
+/*
+function postToGoogle() {
+       var field1 = $("#firstnameField").val();
+       var field2 = $("#lastnameField").val();
+       var field3 = $("#emailField").val();
+       var field4 = $("#descField").val();
+   
+       /*if(field1 == ""){
+           alert('Please Fill Your Name');
+           document.getElementById("firstnameField").focus();
+           return false;
+       }
+       if(field2 == ""){
+           alert('Please Fill Your Email');
+           document.getElementById("lastnameField").focus();
+           return false;
+       }
+       if(field3 == "" || field3.length > 10 || field3.length < 10){
+           alert('Please Fill Your Mobile Number');
+           document.getElementById("emailField").focus();
+           return false;
+       }
+   
+       $.ajax({
+        url: "https://docs.google.com/forms/d/e/1FAIpQLSdRJsUDRtJ06ktWiAP0-56Lx7surtRbXpQz9bn2vNmzOUGMOA/formResponse?",
+        data: {"entry.1400226010": field1, "entry.1628362279": field2, "entry.1569788439": field3, "entry.560530074": field4},
+        type: "POST",
+        dataType: "xml",
+        success: function(d)}
+        {
+        },
+        error: function(x, y, z)
+            {
+    
+                $('#success-msg').show();
+                $('#form').hide();
+    
+            }
+    });
+    return false;
+    }
+*/
+
 var index = 0;
 function next()
 {
@@ -76,7 +119,7 @@ function next()
   count++;
   carousel.style.transform = 'translateX(' + (-size * count) + 'px)';
   
-  setTimeout(next, 2000);
+  setTimeout(next, 3000);
 }
 next();
 
@@ -92,3 +135,5 @@ carousel.addEventListener('transitionend',()=>{
     carousel.style.transform = 'translateX(' + (-size * count) + 'px)';
   }
 });
+
+
